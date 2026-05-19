797f9703fce9ef7ab3b9cafb3733a6dd2ebf52a1
NinaMiyazaki <24215@burnside.school.nz>
2026-05-19 12:25:21 +1200

    finished function, started main code

--- app.py
+++ app.py
@@ -13,8 +13,17 @@ def print_all_teacher_info():
         sql = "SELECT teacher_name, main_subject FROM teacher;"
         cursor.execute(sql)
         results = cursor.fetchall()
-        #
-        print(results)
-
-
-
+        # Prints results clearly
+        print('TEACHER NAME   MAIN SUBJECT')
+        for info in results:
+            print(f'{info[0]: <15}{info[1]}')
+       
+# Main code
+if __name__ == "__main__":
+    choice = input(
+    '''
+    What would you like to do?
+    1. Print all teacher information
+    ''')
+    if choice == '1':
+        print_all_teacher_info()
